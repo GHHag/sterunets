d6c79a1211cfc78f72660a23d6a5dcc8f2a76692
GHHag <gustav.hagenblad@gmail.com>
2023-10-11 21:46:12 +0200

    fleshing

---
+++ steru/steru.go
@@ -0,0 +1,15 @@
+package steru
+
+import (
+	// "context"
+	// "net"
+	// "google.golang.org/grpc"
+)
+
+type SteruType struct {
+
+}
+
+type DataMapping struct {
+
+}
---
+++ test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+
+# 1.
+# Import data
+# Create dataframe
+# Create/calculate feature
+# Create model
+
+# 2.
+# Statefully store model + dataframe
+# Add new data
+# Calculate features
+# Make predictions
