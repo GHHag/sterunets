0d4130cde6794cb2acf7c6770a3641b63cfd5ff1
GHHag <gustav.hagenblad@gmail.com>
2024-01-20 22:22:27 +0100

    feature blueprint class

--- sterunets/data_handler.py
+++ sterunets/data_handler.py
@@ -6,27 +6,57 @@ import pandas as pd
 # import numpy as np
 
 
-class FeaturesBlueprint:
-
-    def __init__(self,  feature_funcs: list[callable]):
-        self.__feature_funcs: list[tuple[callable, tuple, int]] = feature_funcs
-
-    def __call__(self, dataframe: pd.DataFrame):
-        for func, arguments, required_periods in self.__feature_funcs:
-            func(dataframe[-required_periods:], *arguments)
-
-
-# pandas DataFrame wrapper class
-class DataHandler:
+class FeatureBlueprint:
 
     def __init__(
-        self, features_blueprint: FeaturesBlueprint,
-        raw_data_fields: dict[str, type], dataframe: pd.DataFrame
+        self, name: str, feature_func: callable, 
+        arguments: tuple, required_periods: int
     ):
-        self.__features_blueprint: FeaturesBlueprint = features_blueprint
+        self.__name: str = name
+        self.__feature_func: callable = feature_func
+        self.__arguments: tuple = arguments
+        self.__required_periods: int = required_periods
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def feature_func(self) -> callable:
+        return self.__feature_func
+
+    @property
+    def arguments(self) -> tuple:
+        return self.__arguments
+
+    @property
+    def required_periods(self) -> int:
+        return self.__required_periods
+
+    def __call__(self, dataframe: pd.DataFrame):
+        return self.__feature_func(
+            dataframe.iloc[:, -self.__required_periods:], 
+            *self.__arguments
+        )
+
+
+class DataHandler:
+
+    __feature_blueprints: list[FeatureBlueprint] = []
+
+    def __init__(
+        self, raw_data_fields: dict[str, type], dataframe: pd.DataFrame
+    ):
         self.__raw_data_fields: dict[str, type] = raw_data_fields
         self.__feature_counter: Counter = Counter(raw_data_fields.keys())
         self.__dataframe: pd.DataFrame = dataframe
+
+    @property
+    def dataframe(self):
+        return self.__dataframe if self.__dataframe is not None else None
+
+    def add_feature_blueprint(self, feature_blueprint: FeatureBlueprint):
+        self.__feature_blueprints.append(feature_blueprint)
 
     def add_json_data(self, json_data: json):
         loaded_json_data: dict = json.loads(json_data)
@@ -39,27 +69,28 @@ class DataHandler:
                     f'"{field}" is not of the expected type "{data_type}".'
                 )
 
-        new_data: pd.DataFrame = pd.read_json(
-            loaded_json_data, orient='records'
-        )
+        columns = self.__dataframe.columns
+        # TODO: Evaluate if possible to use from_dict method instead to
+        # create the new dataframe here.
+        new_data = pd.DataFrame([loaded_json_data], columns=columns)
         self.__dataframe = pd.concat(
             [self.__dataframe, new_data], ignore_index=True
         )
 
-        # return self.__dataframe # fluent style or not?
-
     def apply_features(self):
-        self.__features_blueprint(self.__dataframe)
+        for feature_blueprint in self.__feature_blueprints:
+            # self.__dataframe = feature_blueprint(self.__dataframe)
+            feature_blueprint(self.__dataframe)
+        print(self.__dataframe)
 
 
 class TimeSeriesDataHandler(DataHandler):
 
     def __init__(
-        self, features_blueprint: FeaturesBlueprint,
-        raw_data_fields: dict[str, type], dataframe: pd.DataFrame,
+        self, raw_data_fields: dict[str, type], dataframe: pd.DataFrame,
         start_dt: dt.datetime, end_dt: dt.datetime
     ):
-        super().__init__(features_blueprint, raw_data_fields, dataframe)
+        super().__init__(raw_data_fields, dataframe)
         self.__start_dt: dt.datetime = start_dt
         self.__end_dt: dt.datetime = end_dt
 
--- test.py
+++ test.py
@@ -1,12 +1,15 @@
+import datetime as dt
+import json
+
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import train_test_split
+from sklearn.model_selection import train_test_split
 
-from nets.model_handler import ModelHandler
-from nets.data_handler import DataHandler
-from nets.grpc_service import *
-from nets.grpc_service import run_grpc_server
+from sterunets.model_handler import ModelHandler
+from sterunets.data_handler import DataHandler, FeatureBlueprint
+from sterunets.grpc_service import *
+from sterunets.grpc_service import run_grpc_server
 
 # 1.
 # Import data + create dataframe
@@ -20,32 +23,42 @@ from nets.grpc_service import run_grpc_server
 # Make predictions
 
 
-def feature_x(df):
-    pass
-
-
-def feature_y(df):
-    pass
-
-
-def apply_features(df):
-    pass
-
-
 def persist_model(model):
     pass
 
 
-def main():
-    df = pd.read_csv('some_csv_file.csv')
+def calculate_mean(df):
+    df['mean_value'] = df['value'].mean()
+    # return df
 
-    apply_features(df)
 
-    # Pass df to train_test_split or convert to numpy data structure first?
-    X_train, y_train, X_test, y_test = train_test_split(df)
-
-    model = LinearRegression()
+def calculate_rolling_mean(df, periods):
+    df['rolling_mean_value'] = df['value'].rolling(periods).mean()
+    # return df
 
 
 if __name__ == '__main__':
-    main()
+    raw_data_fields = {'dummy_column': str, 'value': float}
+    initial_data = {
+        'dummy_column': ['a', 'b', 'c', 'd'],
+        'value': [10, 15, 15, 20]
+    }
+    initial_df = pd.DataFrame(initial_data)
+    data_handler = DataHandler(raw_data_fields, initial_df)
+
+    feature_blueprint = FeatureBlueprint('mean_value', calculate_mean, (), 2)
+    feature_blueprint1 = FeatureBlueprint(
+        'rolling_mean_value', calculate_rolling_mean, (2,), 2
+    )
+    data_handler.add_feature_blueprint(feature_blueprint)
+    # data_handler.add_feature_blueprint(feature_blueprint1)
+
+    data_handler.apply_features()
+
+    new_data = {'dummy_column': 'e', 'value': 22.5}
+    json_data = json.dumps(new_data)
+    data_handler.add_json_data(json_data)
+
+    data_handler.apply_features()
+
+    print(data_handler.dataframe)
